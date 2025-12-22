d316469f429ad19ab0a0ea8d6c16c86afa632e32
Henry Vo <henryhuyvo74@gmail.com>
2025-12-22 00:06:02 -0600

    Initialized Data

---
+++ Queries/EDA.sql
@@ -0,0 +1,23 @@
+USE New_Prod;
+
+
+-- Analysis Window (yyyy-mm-dd) 2023-01-01 - 2024-12-31
+-- Product Release Date: 2023-09-15
+-- Allow sufficient pre and post release data to evaluate business impact 
+
+
+--SELECT
+--MIN(signup_date) AS first_date,
+--MAX(signup_date) AS last_date
+--FROM dbo.ravenstack_accounts
+
+--SELECT
+--MIN(submitted_at) AS first_date,
+--MAX(submitted_at) AS last_date
+--FROM dbo.ravenstack_support_tickets
+
+--SELECT *
+--FROM dbo.ravenstack_support_tickets
+
+SELECT *
+FROM dbo.customer_churn_business_dataset
